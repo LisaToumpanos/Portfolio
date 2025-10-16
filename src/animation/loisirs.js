e501534e6749694b827e3e1b92eba2fb8dc94771
Lisa Toumpanos <Lisa.Toumpanos@etu.univ-Grenoble-Alpes.fr>
2025-10-16 23:43:13 +0200

    ajout du résumé de Tokyo Revenger

--- src/animation/loisirs.js
+++ src/animation/loisirs.js
@@ -3,6 +3,9 @@
 const showBtn = document.getElementById('showBtn');
 const hideBtn = document.getElementById('hideBtn');
 const extra = document.getElementById('extra');
+const resumeBtn = document.getElementById('resumeBtn');
+const TokyoRevenger = document.getElementById('TokyoRevenger');
+const cacher = document.getElementById('cacher');
 
 hideBtn.style.display ="none";
 
@@ -16,4 +19,17 @@ hideBtn.addEventListener('click',() => {
 	extra.style.display = 'none';
 	hideBtn.style.display ="none";
 	showBtn.style.display ="inline-block";
+})
+
+resumeBtn.addEventListener('click',() => {
+	TokyoRevenger.style.display = 'block';
+	resumeBtn.style.display ="none";
+	cacher.style.display ="inline-block";
+})
+
+
+cacher.addEventListener('click',() => {
+	TokyoRevenger.style.display = 'none';
+	cacher.style.display ="none";
+	resumeBtn.style.display ="inline-block";
 })
